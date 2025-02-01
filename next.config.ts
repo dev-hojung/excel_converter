0b8730da99a9e7c17386065672e0c34b125b94d5
IMSHoJung <IMSHoJung@imsmobility.co.kr>
2025-02-01 18:13:19 +0900

    fix: any type

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   api: {
-    bodyParser: false, // Next.js의 기본 bodyParser를 사용하지 않도록 설정
+    bodyParser: {
+      sizeLimit: '50mb', // 기본값: 4.5MB → 10MB로 증가
+    },
   },
 };
 
